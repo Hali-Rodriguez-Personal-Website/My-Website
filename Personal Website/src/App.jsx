54d840fbdc3f9e370b1fefe9da72cd4b139a4365
Hali <128436801+Shaberryy@users.noreply.github.com>
2025-03-08 21:48:46 -0800

    hm

--- Personal Website/src/App.jsx
+++ Personal Website/src/App.jsx
@@ -1,5 +1,7 @@
 /** @format */
 
+// /** @format */
+
 // import { useState } from "react";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
--- Personal Website/src/components/About.jsx
+++ Personal Website/src/components/About.jsx
@@ -1,4 +1,6 @@
-import NavBar from "./NavBar";
+/** @format */
+
+// import NavBar from "./NavBar";
 // import { Link } from "react-router-dom";
 import ContactPage from "./ContactPage";
 export default function About() {
